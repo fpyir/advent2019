a42ba54cd6a6b1963a8ea86f55bb4833ad629fac
Adam Marshall <adam010101@icloud.com>
2019-12-16 20:55:23 +1100

    black

--- 01_1.py
+++ 01_1.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     with open("01_1__input.txt", "r") as modules:
         for module in modules:
             total_fuel_required += calculate_fuel_required(int(module))
-    print(f"total fuel required: {total_fuel_required}") 
+    print(f"total fuel required: {total_fuel_required}")
