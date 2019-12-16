f26f27b2648aaec6a1d064c23ac193e488c4213d
Adam Marshall <adam010101@icloud.com>
2019-12-16 20:54:42 +1100

    python solution

---
+++ 01_1.py
@@ -0,0 +1,13 @@
+import math
+
+
+def calculate_fuel_required(module_mass):
+    return math.floor(module_mass / 3) - 2
+
+
+if __name__ == "__main__":
+    total_fuel_required = 0
+    with open("01_1__input.txt", "r") as modules:
+        for module in modules:
+            total_fuel_required += calculate_fuel_required(int(module))
+    print(f"total fuel required: {total_fuel_required}") 
